8528b7fc8857e8811349f17d7913ef755ee28c6f
matthewstasiak97 <matthewstasiak97@gmail.com>
2025-04-24 09:35:41 -0400

    Show.jsx added and started Create events form

---
+++ src/components/Show/Show.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { show } from "../../../services/userService.js";
+
+function Show() {
+  const { id } = useParams();
+  const [person, setPerson] = useState({});
+
+  const fetchPerson = async () => {
+    try {
+      const fetchedPerson = await show(id);
+      setPerson(fetchedPerson);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPerson();
+  }, []);
+
+  return (
+    <div>
+      <h1>{person.user ? person.user.username : "Please Log in."}</h1>
+    </div>
+  );
+}
+
+export default Show;

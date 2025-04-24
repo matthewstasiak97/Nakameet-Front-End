10a96edd18d83292b7bf167e7db62cb083fc329e
bonnie <owu.owu.666@gmail.com>
2025-04-24 11:51:23 -0400

    fixed navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,59 +1,43 @@
-import { useContext } from "react";
-import { Link, useNavigate } from "react-router";
-import { UserContext } from "../../contexts/UserContext";
+import { Link, useNavigate } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
-  const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
 
-  const handleSignOut = () => {
-    localStorage.removeItem("token");
-    setUser(null);
-    navigate("/");
+  const handleClick = (path) => {
+    navigate(path);
   };
 
   return (
     <nav className="navbar">
-      {user ? (
-        <ul className="navbar-list">
-          <li className="navbar-item welcome-text">Welcome, {user.username}</li>
-          <li className="navbar-item">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <button 
+            className="navbar-button btn-events"
+            onClick={() => handleClick('/events')}
+          >
             Events
-            <Link to="/">
-              <button className="navbar-button btn-events">Events</button>
-            </Link>
-          </li>
-          <li className="navbar-item">
-            <button
-              onClick={handleSignOut}
-              className="navbar-button btn-signout"
-            >
-              Sign Out
-            </button>
-          </li>
-        </ul>
-      ) : (
-        <ul className="navbar-list">
-          <li className="navbar-item">
-            <Link to="/events">
-              <button className="navbar-button btn-events">Events</button>
-            </Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/sign-up">
-              <button className="navbar-button btn-signup">Sign up</button>
-            </Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/sign-in">
-              <button className="navbar-button btn-signin">Sign in</button>
-            </Link>
-          </li>
-        </ul>
-      )}
+          </button>
+        </li>
+        <li className="navbar-item">
+          <button 
+            className="navbar-button btn-signup"
+            onClick={() => handleClick('/signup')}
+          >
+            Sign up
+          </button>
+        </li>
+        <li className="navbar-item">
+          <button 
+            className="navbar-button btn-signin"
+            onClick={() => handleClick('/signin')}
+          >
+            Sign in
+          </button>
+        </li>
+      </ul>
     </nav>
-);
+  );
 }
 
 export default NavBar;

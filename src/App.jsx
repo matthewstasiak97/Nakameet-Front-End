2c3c711e7f7c1b1639db29fa0857fab67fc1c2bc
bonnie <owu.owu.666@gmail.com>
2025-04-25 14:58:48 -0400

    add event button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { useContext, useState } from "react";
-//import { Routes, Route } from "react-router";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import SignUpForm from "./components/SignUpForm/SignUpForm.jsx";
 import SignInForm from "./components/SignInForm/SignInForm.jsx";
-import Event from "./components/Events/CreateEvents.jsx";
+import EventDetails from "./components/EventDetail/EventDetail.jsx";
+import CreateEvents from "./components/Events/CreateEvents.jsx";
 import Home from "./components/home/home.jsx";
 import Show from "./components/Show/Show.jsx";
 import { UserContext } from "./contexts/UserContext";
@@ -24,9 +23,11 @@ const App = () => {
             <Route path="/sign-up" element={<SignUpForm />} />
             <Route path="/sign-in" element={<SignInForm />} />
             <Route path="/events/:id" element={<EventDetail />} />
-            {/* <Route path="/events/:id/edit" element={<EditEvent />} />
-            <Route path="/events/:id/delete" element={<DeleteEvent />} />
-            <Route path="/events/create" element={<CreateEvents />} /> */}
+            <Route path="/events/new" element={<CreateEvents />} />
+            {/* <Route path="/events/create" element={<CreateEvents />} />
+          <Route path="/events/:id" element={<EventDetails />} />
+          <Route path="/events/:id/edit" element={<EditEvent />} />
+          <Route path="/events/:id/delete" element={<DeleteEvent />} /> */}
           </Routes>
         </div>
       </div>
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { Router } from "react-router";
-import { useState } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom"; 
 import EventBar from "../EventBar/eventBar";
 import SearchBar from "../SearchBar/searchBar";
 
 function Home() {
   const [searchText, setSearchText] = useState("");
+
   return (
     <div>
-      home
+      <h1>Home</h1>
+      <Link to="/events/new">
+        <button>Add Event</button>
+      </Link>
+
       <SearchBar value={searchText} onSearchChange={setSearchText} />
       <EventBar searchText={searchText} />
     </div>
